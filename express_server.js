ec07a671f8b12df99069470fc985410cbb68c563
sarahmanhard <smanhard88@gmail.com>
2023-06-25 20:27:25 -0700

    add a second route

--- express_server.js
+++ express_server.js
@@ -29,3 +29,10 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  const templateVars = { id, longURL };
+  res.render("urls_show", templateVars);
+});

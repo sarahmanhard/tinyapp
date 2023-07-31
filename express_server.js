51f79727e022e3af2dc188ae5f666482573d0d2f
sarahmanhard <smanhard88@gmail.com>
2023-07-31 04:29:26 -0700

    refractored to meet project requirments

--- express_server.js
+++ express_server.js
@@ -5,8 +5,6 @@ const path = require('path');
 const { getUserByEmail, generateRandomString } = require("./helpers");
 const { urlDatabase, users } = require("./database");
 
-
-
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -21,9 +19,7 @@ app.use(
 const bcrypt = require("bcrypt");
 
 app.set("view engine", "ejs");
-
 app.set('views', path.join(__dirname, 'views'));
-
 
 // Helper functions 
 // Function to filter and return URLs associated with a specific user ID
@@ -47,92 +43,8 @@ const requireLogin = (req, res, next) => {
     next();
   }
 };
+
 // Routes
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls", (req, res) => {
-  const userId = req.session.user_id;
-  if (userId) {
-    const userUrls = urlsForUser(userId);
-    res.render("urls_index", { urls: userUrls, user: users[userId] });
-  } else {
-    res.render("urls_index", { urls: {}, user: null }); // Pass an empty object for urls when the user is not logged in
-  }
-});
-
-
-app.get("/urls/new", requireLogin, (req, res) => {
-  const userId = req.session.user_id;
-  const user = users[userId];
-  const templateVars = {
-    user,
-  };
-  res.render("urls_new", templateVars);
-});
-
-app.get("/urls/:id/edit", (req, res) => {
-  const shortURL = req.params.id;
-  const url = urlDatabase[shortURL];
-
-  if (!url) {
-    res.status(404).send("URL not found");
-    return;
-  }
-
-  const userId = req.session.user_id;
-  const user = users[userId];
-  const templateVars = {
-    user,
-    shortURL,
-    longURL: urlDatabase[shortURL].longURL,
-  };
-
-  res.render("urls_edit", templateVars);
-});
-
-app.post("/urls", requireLogin, (req, res) => {
-  const userId = req.session.user_id;
-  const user = users[userId];
-
-  const shortURL = generateRandomString();
-  const longURL = req.body.longURL;
-  urlDatabase[shortURL] = {
-    longURL,
-    userID: user.id,
-  };
-
-  res.redirect("/urls");
-});
-
-app.post("/urls/:id/delete", (req, res) => {
-  const shortURL = req.params.id;
-  delete urlDatabase[shortURL];
-  res.redirect("/urls");
-});
-
-
-app.get("/urls/:id", (req, res) => {
-  const shortURL = req.params.id;
-  const url = urlDatabase[shortURL];
-
-  if (!url) {
-    res.status(404).send("URL not found");
-    return;
-  }
-
-  const userId = req.session.user_id;
-  const user = users[userId];
-  const templateVars = {
-    user,
-    shortURL,
-    longURL: urlDatabase[shortURL].longURL,
-  };
-  res.render("urls_show", templateVars);
-});
-
 app.get("/", (req, res) => {
   const userId = req.session.user_id;
   if (userId) {
@@ -142,15 +54,121 @@ app.get("/", (req, res) => {
   }
 });
 
+app.get("/urls", requireLogin, (req, res) => {
+  const userId = req.session.user_id;
+  const userUrls = urlsForUser(userId);
+  const templateVars = {
+    urls: userUrls,
+    user: users[userId],
+  };
+  res.render("urls_index", templateVars);
+});
 
-app.post("/urls/:id", (req, res) => {
+app.get("/urls/new", requireLogin, (req, res) => {
+  const userId = req.session.user_id;
+  const templateVars = {
+    user: users[userId],
+  };
+  res.render("urls_new", templateVars);
+});
+
+app.get("/urls/:id/edit", requireLogin, (req, res) => {
+  const userId = req.session.user_id;
   const shortURL = req.params.id;
-  const newLongURL = req.body.longURL;
-  
-  // Update the longURL in the database
-  urlDatabase[shortURL].longURL = newLongURL;
+  const url = urlDatabase[shortURL];
+
+  if (!url || url.userID !== userId) {
+    res.status(404).send("URL not found");
+    return;
+  }
+  // Other users will get a access denied message if trying to access another's URL
+  if (url.userID !== userId) {
+    res.status(403).send("Access denied");
+    return;
+  }
+
+
+  const templateVars = {
+    user: users[userId],
+    shortURL,
+    longURL: url.longURL,
+  };
+
+  res.render("urls_edit", templateVars);
+});
+
+app.post("/urls", requireLogin, (req, res) => {
+  const userId = req.session.user_id;
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+
+  urlDatabase[shortURL] = {
+    longURL,
+    userID: userId,
+  };
 
   res.redirect("/urls");
+});
+
+app.post("/urls/:id", requireLogin, (req, res) => {
+  const userId = req.session.user_id;
+  const shortURL = req.params.id;
+  const url = urlDatabase[shortURL];
+
+  if (!url || url.userID !== userId) {
+    res.status(403).send("Access denied");
+    return;
+  }
+
+  const newLongURL = req.body.longURL;
+  url.longURL = newLongURL;
+
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id/delete", requireLogin, (req, res) => {
+  const userId = req.session.user_id;
+  const shortURL = req.params.id;
+  const url = urlDatabase[shortURL];
+
+  if (!url || url.userID !== userId) {
+    res.status(403).send("Access denied");
+    return;
+  }
+
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
+app.get("/urls/:id", requireLogin, (req, res) => {
+  const userId = req.session.user_id;
+  const shortURL = req.params.id;
+  const url = urlDatabase[shortURL];
+
+  if (!url || url.userID !== userId) {
+    res.status(404).send("URL not found");
+    return;
+  }
+
+  const templateVars = {
+    user: users[userId],
+    shortURL,
+    longURL: url.longURL,
+  };
+
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const url = urlDatabase[shortURL];
+
+  if (!url) {
+    res.status(404).send("URL not found");
+    return;
+  }
+
+  res.redirect(url.longURL);
 });
 
 app.get("/register", (req, res) => {
@@ -187,23 +205,17 @@ app.post("/register", (req, res) => {
 
 app.get("/login", (req, res) => {
   const userId = req.session.user_id;
-  const user = users[userId];
   const templateVars = {
-    user,
+    user: users[userId],
   };
-  res.render("urls_login.ejs", templateVars);
+  res.render("urls_login", templateVars);
 });
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
   const user = getUserByEmail(email);
 
-  if (!user) {
-    res.status(403).send("Invalid login credentials");
-    return;
-  }
-
-  if (!bcrypt.compareSync(password, user.password)) {
+  if (!user || !bcrypt.compareSync(password, user.password)) {
     res.status(403).send("Invalid login credentials");
     return;
   }
